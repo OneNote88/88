9d5ad6d2ee7c811286e77a1673cfdae21a2fc7f5
OneNote88 <vanyaginanton@gmail.com>
2025-04-09 14:56:43 +0300

    Add files via upload

---
+++ 1232132132131232132131232131/1232132132131232132131232131/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _1232132132131232132131232131
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
